072aa5c924962da28f9a1871f0fe2aa4d629acaf
Suryansh Purohit <suryansh.purohit3112@gmail.com>
2020-07-14 18:03:56 +0530

    Basic Examination page

--- studentbackend/app.js
+++ studentbackend/app.js
@@ -1,34 +1,47 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const PORT = 5000
-const {MONGOURI} = require('./keys')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = 5000;
+const { MONGOURI } = require('./keys');
 
-const profileRoutes = require('./routes/profileDetails')
-const prevDataRoutes = require('./routes/prevData')
-const relationRoutes = require('./routes/relation')
+const profileRoutes = require('./routes/profileDetails');
+const prevDataRoutes = require('./routes/prevData');
+const relationRoutes = require('./routes/relation');
 
-require('./models/project')
-require('./models/assignment')
-require('./models/internship')
-require('./models/recommendation')
+require('./models/project');
+require('./models/assignment');
+require('./models/internship');
+require('./models/recommendation');
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(require('./routes/project'))
-app.use(require('./routes/assignment'))
-app.use(require('./routes/internship'))
-app.use(require('./routes/recommendation'))
+app.use(require('./routes/project'));
+app.use(require('./routes/assignment'));
+app.use(require('./routes/internship'));
+app.use(require('./routes/recommendation'));
 
-app.use('/profile',profileRoutes)
-app.use('/prevData',prevDataRoutes)
-app.use('/find',relationRoutes)
-app.use('/uploads',express.static('../Student/uploads'))
+app.use('/profile', profileRoutes);
+app.use('/prevData', prevDataRoutes);
+app.use('/find', relationRoutes);
+app.use('/uploads', express.static('../Student/uploads'));
 
-mongoose.connect(MONGOURI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(() => console.log('MongoDB Connected...'))
-    .catch((err) => console.log(err))
+///////
 
-app.listen(PORT,()=>{
-    console.log("server is running on ",PORT)
-})
+const gradeRoute = require('./routes/grade');
+
+app.use('/grade', gradeRoute);
+
+///////
+
+mongoose
+  .connect(MONGOURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log('server is running on ', PORT);
+});
--- studentbackend/keys.js
+++ studentbackend/keys.js
@@ -1,4 +1,6 @@
-module.exports={
-    MONGOURI : 'mongodb+srv://Apoorv:1410@cluster0.mqdbn.gcp.mongodb.net/test?retryWrites=true&w=majority',
-    JWT_SECRET : "<any random string>"
-}
+module.exports = {
+  MONGOURI: 'mongodb://localhost:27017/student-backend',
+  JWT_SECRET: '<any random string>'
+};
+
+//'mongodb+srv://Apoorv:1410@cluster0.mqdbn.gcp.mongodb.net/test?retryWrites=true&w=majority'
