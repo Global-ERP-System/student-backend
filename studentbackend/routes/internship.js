ce20b98bdc9dff7a4cc197544f8e013589bfab40
kushagra <kushagrabansal900@gmail.com>
2020-07-15 20:05:52 +0530

    added auth in internship page

--- studentbackend/routes/internship.js
+++ studentbackend/routes/internship.js
@@ -1,38 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
-// as projects are protected resource hence middleware is neccessary
-//const requiredLogin = require('../middleware/requirelogin')
+const requirelogin = require('../middleware/requirelogin')
 const Internship = mongoose.model('Internship')
 const InternshipData = mongoose.model('InternshipData')
 
-//Here i will get a user response it can be a student,teacher or master
 
-router.post('/createinternship',(req,res)=>{
-    const {currentlyavailable, preference, skills, cv} = req.body
-    if(!currentlyavailable || !preference || !skills || !cv){
-        return res.status(422).json({error : "please add all fields"})
-    }
-    //req.user.password = undefined
-    const internshipData = new InternshipData({
-        currentlyavailable,
-        preference,
-        skills,
-        cv,
-        postedBy : req.user
-    })
-    internshipData.save().then(result=>{
-        res.json({internshipdata:result})
-    })
-    .catch(err=>{
-        console.log(err)
-    })
-
+router.post('/createinternship',requirelogin,(req,res)=>{
     const {nameofthecompany, position, startingdate, enddate} = req.body
     if(!nameofthecompany || !position || !startingdate || !enddate){
         return res.status(422).json({error : "please add all fields"})
     }
-    //req.user.password = undefined
+
+    req.user.password = undefined
     const internship = new Internship({
         nameofthecompany,
         position,
@@ -47,16 +27,11 @@ router.post('/createinternship',(req,res)=>{
         console.log(err)
     })    
 })
+
 // _id and name are fields in student table 
-router.get('/myinternship',(req,res)=>{
-    Internship.find() 
-    .then(myinternship=>{
-        res.json({myinternship})
-    })
-    .catch(err=>{
-        console.log(err)
-    })
-    InternshipData.find() 
+router.get('/myinternship',requirelogin,(req,res)=>{
+    Internship.find({postedBy: req.user._id}) 
+    .populate('postedBy','_id name')
     .then(myinternship=>{
         res.json({myinternship})
     })
